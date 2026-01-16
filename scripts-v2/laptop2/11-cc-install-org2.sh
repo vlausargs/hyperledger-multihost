0c5e6ae0f188e77529c4f69f2f543b5164de5891
valospc1 <valospc1@valos.id>
2026-01-16 19:01:36 +0700

    up

---
+++ scripts-v2/laptop2/11-cc-install-org2.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -euo pipefail
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
+source "$ROOT_DIR/scripts-v2/00-env.sh"
+
+peer_env_org2
+peer lifecycle chaincode install "channel-artifacts/${CC_NAME}.tar.gz"
